c7b3b23f21f97be7400e98a2d63750ddef67f296
lakshitasoniii36-hash <lakshitasoniii36@gmail.com>
2026-05-27 18:04:21 +0530

    member master updatess

--- Shree ram Bootstrap webbase Software/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Shree ram Bootstrap webbase Software/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f521bae2e0d76145b4ff5d4b5e30669c6719457")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a35b35beb16abc5f7793f524ec4f2955b212d8db")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
